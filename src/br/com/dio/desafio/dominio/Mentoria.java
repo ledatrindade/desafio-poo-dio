8d9f47f452c9129abcdf6e3b34f09be6deadaf5b
Lêda Trindade <ledatrindade94@gmail.com>
2026-01-08 18:19:48 -0300

    Add Mentoria content type

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,44 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+/**
+ * Representa uma mentoria.
+ * Diferente do curso, não depende de carga horária,
+ * mas possui uma data específica.
+ */
+public class Mentoria extends Conteudo {
+
+    // Data em que a mentoria acontece
+    private LocalDate data;
+
+    /**
+     * Regra de XP fixa para mentoria,
+     * pois o valor está mais ligado à experiência
+     * do que à duração.
+     */
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    /**
+     * Facilita a leitura das informações no console.
+     */
+    @Override
+    public String toString() {
+        return "Mentoria{" +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
+                ", data=" + data +
+                '}';
+    }
+}
